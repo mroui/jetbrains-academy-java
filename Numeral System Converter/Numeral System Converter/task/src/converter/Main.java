8ec1cb0895c589b15472894a29375bc9878858e6
Martyna Roj <martynaxroj@gmail.com>
2020-07-12 22:44:23 +0200

    [MODIFY] add converting fractions functionality with converting fractional to dec & dec to any radix

--- Numeral System Converter/Numeral System Converter/task/src/converter/Main.java
+++ Numeral System Converter/Numeral System Converter/task/src/converter/Main.java
@@ -5,7 +5,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        System.out.println(NumeralConverter.fromToAnyRadix(scanner.nextInt(), scanner.next(), scanner.nextInt()));
+        int sourceRadix = scanner.nextInt();
+        String number = scanner.next();
+        int targetRadix = scanner.nextInt();
+        System.out.println(NumeralConverter.fromToAnyRadix(sourceRadix, number, targetRadix));
         scanner.close();
     }
 }
--- Numeral System Converter/Numeral System Converter/task/src/converter/NumeralConverter.java
+++ Numeral System Converter/Numeral System Converter/task/src/converter/NumeralConverter.java
@@ -2,7 +2,7 @@ package converter;
 
 public abstract class NumeralConverter {
 
-    public static String decToBin(int number) {
+    public static String decToBin(long number) {
         StringBuilder binary = new StringBuilder();
         if (number == 0)
             binary.append(0);
@@ -18,16 +18,45 @@ public abstract class NumeralConverter {
         return (radix == 2 ? "0b" : radix == 8 ? "0" : radix == 16 ? "0x" : "") + Long.toString(number, radix);
     }
 
-    private static String decTo1(long number) {
+    public static String decTo1(long number) {
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < number; i++)
             result.append(1);
         return result.toString();
     }
 
+    public static String fractionalToDec(String number, int radix) {
+        String[] numbers = number.substring(number.indexOf('.') + 1).split("");
+        double result = 0;
+        for (int i = 0; i < numbers.length; i++)
+            result += Long.parseLong(numbers[i], radix) / Math.pow(radix, i + 1);
+        return String.valueOf(result);
+    }
+
+    public static String fractionalDecToAny(double number, int radix) {
+        StringBuilder result = new StringBuilder("0.");
+        for (int i = 0; i < 5; i++) {
+            double x = number * radix;
+            result.append(Integer.toString((int) x, radix));
+            number = x - (int) x;
+        }
+        return result.toString();
+    }
+
     public static String fromToAnyRadix(int radixFrom, String number, int radixTo) {
-        long decimal = radixFrom == 1 ? number.length() : Long.parseLong(number, radixFrom);
-        return radixTo == 1 ? decTo1(Long.parseLong(number)) : Long.toString(decimal, radixTo);
+        String integer = number.contains(".") ? number.substring(0, number.indexOf('.')) : number;
+        String fractional = number.contains(".") ? "0" + number.substring(number.indexOf('.')) : "";
+
+        long decimalInteger = radixFrom == 1 ? integer.length() : Long.parseLong(integer, radixFrom);
+        String integerPart = radixTo == 1 ? decTo1(Long.parseLong(integer)) : Long.toString(decimalInteger, radixTo);
+
+        String fractionalPart = "";
+        if (!fractional.equals("")) {
+            double decimalFractional = Double.parseDouble(fractionalToDec(fractional, radixFrom));
+            fractionalPart = radixTo == 1 ? "" : fractionalDecToAny(decimalFractional, radixTo);
+        }
+
+        return radixTo == 1 || fractional.equals("") ? integerPart : integerPart + fractionalPart.substring(fractional.indexOf('.'));
     }
 
 }
