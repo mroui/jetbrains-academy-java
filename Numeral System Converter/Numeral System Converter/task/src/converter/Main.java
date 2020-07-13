dab099ce75cf29beb6c3f1dbb819de9143e965a2
Martyna Roj <martynaxroj@gmail.com>
2020-07-13 15:21:46 +0200

    [MODIFY] Numeral System Converter project - implement handling exceptions by try catch with parsing, radix range

--- Numeral System Converter/Numeral System Converter/task/src/converter/Main.java
+++ Numeral System Converter/Numeral System Converter/task/src/converter/Main.java
@@ -4,11 +4,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        final Scanner scanner = new Scanner(System.in);
-        int sourceRadix = scanner.nextInt();
-        String number = scanner.next();
-        int targetRadix = scanner.nextInt();
-        System.out.println(NumeralConverter.fromToAnyRadix(sourceRadix, number, targetRadix));
-        scanner.close();
+        Scanner scanner = new Scanner(System.in);
+        try {
+            int sourceRadix = scanner.nextInt();
+            String number = scanner.next();
+            int targetRadix = scanner.nextInt();
+            if (sourceRadix < 1 || sourceRadix > 36 || targetRadix < 1 || targetRadix > 36)
+                throw new NumberFormatException("Radix should be in <1, 35>");
+            System.out.println(NumeralConverter.fromToAnyRadix(sourceRadix, number, targetRadix));
+        } catch (Exception e) {
+            System.out.println("error: " + e.getMessage());
+        }
     }
 }
