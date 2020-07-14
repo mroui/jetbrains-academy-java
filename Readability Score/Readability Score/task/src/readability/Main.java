c2d1d2f40d6bad307a9e9c420c1782e03ab8c9a1
Martyna Roj <martynaxroj@gmail.com>
2020-07-14 16:54:01 +0200

    [ADD] estimation if text is hard/easy to read - check length

--- Readability Score/Readability Score/task/src/readability/Main.java
+++ Readability Score/Readability Score/task/src/readability/Main.java
@@ -1,7 +1,13 @@
 package readability;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+        System.out.print(checkTextDifficulty(new Scanner(System.in).nextLine()));
+    }
+
+    private static String checkTextDifficulty(String text) {
+        return text.length() <= 100 ? "EASY" : "HARD";
     }
 }
