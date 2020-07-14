f633cd58549b7211f8a5d7117f763471df8f3975
Martyna Roj <martynaxroj@gmail.com>
2020-07-14 20:14:33 +0200

    [MODIFY] checking difficulty of text by average count words in sentences - using regex

--- Readability Score/Readability Score/task/src/readability/Main.java
+++ Readability Score/Readability Score/task/src/readability/Main.java
@@ -4,10 +4,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print(checkTextDifficulty(new Scanner(System.in).nextLine()));
+        System.out.println(checkTextDifficulty(new Scanner(System.in).nextLine()));
     }
 
-    private static String checkTextDifficulty(String text) {
-        return text.length() <= 100 ? "EASY" : "HARD";
+    public static String checkTextDifficulty(String text) {
+        String[] sentences = text.split("[.?!]");
+        int words = 0;
+        for (String s : sentences)
+            words += s.split("\\s+").length;
+        return words / sentences.length > 10 ? "HARD" : "EASY";
     }
 }
