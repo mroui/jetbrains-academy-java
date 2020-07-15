dbaed70daa3c21ba6249e4f73a501753581ac269
Martyna Roj <martynaxroj@gmail.com>
2020-07-15 17:37:05 +0200

    [ADD] 15.07.2020 daily task - checking if three inputs are different

---
+++ Readability Score/Problems/Check the given numbers are different/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int first = scanner.nextInt();
+        int second = scanner.nextInt();
+        int third = scanner.nextInt();
+        System.out.println(first != second &&  first != third && second != third);
+    }
+}
