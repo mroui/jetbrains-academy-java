d2527cd5c834ac1e6188a8c137349a2ca05c8a7f
Martyna Roj <martynaxroj@gmail.com>
2020-07-16 13:14:37 +0200

    [ADD] Matrix class with addition, printing on std output & loading from std input functionalities & use it to sum 2 matrices

--- Numeric Matrix Processor/Numeric Matrix Processor/task/src/processor/Main.java
+++ Numeric Matrix Processor/Numeric Matrix Processor/task/src/processor/Main.java
@@ -1,7 +1,12 @@
 package processor;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Matrix m1 = new Matrix();
+        Matrix m2 = new Matrix();
+        Matrix m3 = m1.addMatrix(m2);
+        if (m3 != null)
+            m3.print();
     }
 }
---
+++ Numeric Matrix Processor/Numeric Matrix Processor/task/src/processor/Matrix.java
@@ -0,0 +1,55 @@
+package processor;
+
+import java.util.Scanner;
+
+public class Matrix {
+
+    private int rows;
+    private int columns;
+    private int[][] array;
+
+    public Matrix() {
+        load();
+    }
+
+    public Matrix(int rows, int columns, int[][] array) {
+        this.rows = rows;
+        this.columns = columns;
+        this.array = array.clone();
+    }
+
+    private void load() {
+        final Scanner scanner = new Scanner(System.in);
+        try {
+            rows = scanner.nextInt();
+            columns = scanner.nextInt();
+            array = new int[rows][columns];
+            for (int i = 0; i < rows; i++)
+                for (int j = 0; j < columns; j++)
+                    array[i][j] = scanner.nextInt();
+        } catch (NumberFormatException e) {
+            System.out.println(e.getMessage());
+            load();
+        }
+    }
+
+    public void print() {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++)
+                System.out.print(array[i][j] + " ");
+            System.out.println();
+        }
+    }
+
+    public Matrix addMatrix(Matrix m) {
+        if (this.rows != m.rows || this.columns != m.columns) {
+            System.out.println("ERROR");
+            return null;
+        }
+        int[][] newArray = new int[rows][columns];
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < columns; j++)
+                newArray[i][j] = array[i][j] + m.array[i][j];
+        return new Matrix(rows, columns, newArray);
+    }
+}
