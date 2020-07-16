286adfdd032279b7a00b5c8a9351e0aec2b78065
Martyna Roj <martynaxroj@gmail.com>
2020-07-16 16:04:21 +0200

    [MODIFY] add program menu with matrices operations, move loading matrix to main & add multiplications 2 matrices method

--- Numeric Matrix Processor/Numeric Matrix Processor/task/src/processor/Main.java
+++ Numeric Matrix Processor/Numeric Matrix Processor/task/src/processor/Main.java
@@ -4,8 +4,75 @@ import java.util.Scanner;
 
 public class Main {
 
+    final static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Matrix m1 = new Matrix();
-        m1.multiplyConstant(new Scanner(System.in).nextInt()).print();
+        start();
+        scanner.close();
+    }
+
+    private static void start() {
+        System.out.print(
+                "\n1. Add matrices\n" +
+                        "2. Multiply matrix to a constant\n" +
+                        "3. Multiply matrices\n" +
+                        "0. Exit\n" +
+                        "Your choice: ");
+        switch (scanner.next()) {
+            case "1":
+                matricesOperation("ADDITION");
+                break;
+            case "2":
+                multiplyMatrixToConstant();
+                break;
+            case "3":
+                matricesOperation("MULTIPLICATION");
+                break;
+            case "0":
+                return;
+            default:
+                System.out.println("Bad option!");
+        }
+        start();
+    }
+
+    private static void matricesOperation(String operation) {
+        Matrix A = loadMatrix("first");
+        Matrix B = loadMatrix("second");
+        Matrix C = operation.equals("ADDITION") ? A.addMatrix(B) : A.multiplyMatrix(B);
+        if (C != null) {
+            System.out.println("The multiplication result is: ");
+            C.print();
+        }
+    }
+
+    private static void multiplyMatrixToConstant() {
+        Matrix A = loadMatrix("");
+        System.out.print("Enter the constant: ");
+        try {
+            double constant = scanner.nextDouble();
+            System.out.println("The multiplication result is: ");
+            A.multiplyConstant(constant).print();
+        } catch (NumberFormatException e) {
+            System.out.println(e.getMessage());
+            multiplyMatrixToConstant();
+        }
+    }
+
+    private static Matrix loadMatrix(String which) {
+        try {
+            System.out.printf("Enter size of %s matrix: ", which);
+            int rows = scanner.nextInt();
+            int columns = scanner.nextInt();
+            System.out.printf("Enter %s matrix:\n", which);
+            double[][] array = new double[rows][columns];
+            for (int i = 0; i < rows; i++)
+                for (int j = 0; j < columns; j++)
+                    array[i][j] = scanner.nextDouble();
+            return new Matrix(rows, columns, array);
+        } catch (NumberFormatException e) {
+            System.out.println(e.getMessage());
+            return loadMatrix(which);
+        }
     }
 }
--- Numeric Matrix Processor/Numeric Matrix Processor/task/src/processor/Matrix.java
+++ Numeric Matrix Processor/Numeric Matrix Processor/task/src/processor/Matrix.java
@@ -1,36 +1,15 @@
 package processor;
 
-import java.util.Scanner;
-
 public class Matrix {
 
-    private int rows;
-    private int columns;
-    private int[][] array;
+    private final int rows;
+    private final int columns;
+    private final double[][] array;
 
-    public Matrix() {
-        load();
-    }
-
-    public Matrix(int rows, int columns, int[][] array) {
+    public Matrix(int rows, int columns, double[][] array) {
         this.rows = rows;
         this.columns = columns;
         this.array = array.clone();
-    }
-
-    private void load() {
-        final Scanner scanner = new Scanner(System.in);
-        try {
-            rows = scanner.nextInt();
-            columns = scanner.nextInt();
-            array = new int[rows][columns];
-            for (int i = 0; i < rows; i++)
-                for (int j = 0; j < columns; j++)
-                    array[i][j] = scanner.nextInt();
-        } catch (NumberFormatException e) {
-            System.out.println(e.getMessage());
-            load();
-        }
     }
 
     public void print() {
@@ -43,21 +22,34 @@ public class Matrix {
 
     public Matrix addMatrix(Matrix m) {
         if (this.rows != m.rows || this.columns != m.columns) {
-            System.out.println("ERROR");
+            System.out.println("ERROR: wrong dimensions!");
             return null;
         }
-        int[][] newArray = new int[rows][columns];
+        double[][] newArray = new double[rows][columns];
         for (int i = 0; i < rows; i++)
             for (int j = 0; j < columns; j++)
                 newArray[i][j] = array[i][j] + m.array[i][j];
         return new Matrix(rows, columns, newArray);
     }
 
-    public Matrix multiplyConstant(int constant) {
-        int[][] newArray = new int[rows][columns];
+    public Matrix multiplyConstant(double constant) {
+        double[][] newArray = new double[rows][columns];
         for (int i = 0; i < rows; i++)
             for (int j = 0; j < columns; j++)
                 newArray[i][j] = constant * array[i][j];
         return new Matrix(rows, columns, newArray);
+    }
+
+    public Matrix multiplyMatrix(Matrix m) {
+        if (columns != m.rows) {
+            System.out.println("ERROR: wrong dimensions!");
+            return null;
+        }
+        double[][] newArray = new double[rows][m.columns];
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < m.columns; j++)
+                for (int k = 0; k < columns; k++)
+                    newArray[i][j] += array[i][k] * m.array[k][j];
+        return new Matrix(rows, m.columns, newArray);
     }
 }
