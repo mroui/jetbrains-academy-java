5b114ad5614f7d3b1b80f73e17e8746c174f961f
Martyna Roj <martynaxroj@gmail.com>
2020-07-19 20:51:09 +0200

    [ADD] create menu & add Operator class for operations & encode with parity methods

--- Error Correcting Encoder-Decoder/Error Correcting Encoder-Decoder/task/src/correcter/Main.java
+++ Error Correcting Encoder-Decoder/Error Correcting Encoder-Decoder/task/src/correcter/Main.java
@@ -1,73 +1,22 @@
 package correcter;
 
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        try (BufferedReader input = new BufferedReader(new FileReader("send.txt"));
-             BufferedWriter output = new BufferedWriter(new FileWriter("received.txt"))) {
-            output.write(emulateErrorOnBit(input.readLine()));
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
+        showMenu();
+    }
+
+    private static void showMenu() {
+        System.out.print("Write a mode: ");
+        switch (new java.util.Scanner(System.in).next()) {
+            case "encode":
+                Operator.encode("send.txt");
+                break;
+            case "send":
+                break;
+            case "decode":
+                break;
+            default:
+                System.out.println("Bad operation!");
         }
-    }
-
-    private static String emulateErrorOnBit(String text) {
-        StringBuilder result = new StringBuilder();
-        for (char ch : text.toCharArray()) {
-            StringBuilder binary = new StringBuilder(Integer.toBinaryString(ch));
-            int index = random(0, 7);
-            binary.setCharAt(index, binary.charAt(index) == '1' ? '0' : '1');
-            result.append((char) Integer.parseInt(binary.toString(), 2));
-        }
-        return result.toString();
-    }
-
-    private static String decrypt(String message) {
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < message.length(); i += 3)
-            if (message.charAt(i) == message.charAt(i + 1) || message.charAt(i) == message.charAt(i + 2))
-                result.append(message.charAt(i));
-            else if (message.charAt(i + 1) == message.charAt(i + 2))
-                result.append(message.charAt(i + 1));
-        return result.toString();
-    }
-
-    private static String triple(String message) {
-        StringBuilder result = new StringBuilder();
-        for (char ch : message.toCharArray())
-            result.append(ch).append(ch).append(ch);
-        return result.toString();
-    }
-
-    private static String emulateError(String text) {
-        List<Character> alphabet = getAlphabet();
-        StringBuilder result = new StringBuilder(text);
-        for (int i = 0; i < text.length(); i += 3) {
-            int randomIndex = i + random(0, 2);
-            char randomChar = text.charAt(randomIndex);
-            while (text.charAt(randomIndex) == randomChar)
-                randomChar = alphabet.get(random(0, alphabet.size()));
-            result.setCharAt(randomIndex, randomChar);
-        }
-        return result.toString();
-    }
-
-    private static int random(int min, int max) {
-        return (int) (Math.random() * (max - min - 1)) + min;
-    }
-
-    private static List<Character> getAlphabet() {
-        List<Character> alphabet = new ArrayList<>();
-        for (char i = 'A'; i <= 'Z'; i++)
-            alphabet.add(i);
-        for (char i = 'a'; i <= 'z'; i++)
-            alphabet.add(i);
-        for (char i = '0'; i < '9'; i++)
-            alphabet.add(i);
-        alphabet.add(' ');
-        return alphabet;
     }
 }
---
+++ Error Correcting Encoder-Decoder/Error Correcting Encoder-Decoder/task/src/correcter/Operator.java
@@ -0,0 +1,65 @@
+package correcter;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import static correcter.Converter.getWithSpaces;
+import static correcter.Converter.convertToHex;
+
+public abstract class Operator {
+
+    static void encode(String filename) {
+        try (OutputStream writer = new FileOutputStream("encoded.txt")) {
+            String text = new String(Files.readAllBytes(Paths.get(filename)));
+            String hex = convertToHex(text, -1, 1);
+            String bin = Converter.convertToBin(text, 8);
+            StringBuilder twiceBin = new StringBuilder(Converter.repeatCharacters(bin, 2));
+            while (twiceBin.length() % 8 != 0)
+                twiceBin.append('0');
+
+            String encryption = encodeWithParity(twiceBin.toString());
+
+            for (String str : getWithSpaces(encryption, 8).split(" "))
+                writer.write((byte) Integer.parseInt(str, 2));
+
+            System.out.println('\n' + filename + ":\n"
+                    + "text view: " + text + '\n'
+                    + "hex view: " + getWithSpaces(hex, 2) + '\n'
+                    + "bin view: " + getWithSpaces(bin, 8) + "\n\n"
+                    + "encoded.txt:" + '\n'
+                    + "expand: " + getWithSpaces(getExpandFromParity(encryption), 8) + '\n'
+                    + "parity: " + getWithSpaces(encryption, 8) + '\n'
+                    + "hex view: " + getWithSpaces(convertToHex(encryption, 2, 8).toUpperCase(), 2));
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static String encodeWithParity(String text) {
+        StringBuilder encryption = new StringBuilder();
+        int parity = 0;
+        for (int i = 0; i < text.length(); i += 2) {
+            parity = parity ^ Integer.parseInt(text.substring(i, i + 1));
+            encryption.append(text, i, i + 2);
+            if ((i + 2) % 6 == 0) {
+                parity %= 2;
+                encryption.append(parity).append(parity);
+                parity = 0;
+            }
+        }
+        return encryption.toString();
+    }
+
+    private static String getExpandFromParity(String binary) {
+        StringBuilder result = new StringBuilder(binary);
+        for (int i = 6; i < result.length(); i += 8) {
+            result.setCharAt(i, '.');
+            result.setCharAt(i + 1, '.');
+        }
+        return result.toString();
+    }
+
+}
