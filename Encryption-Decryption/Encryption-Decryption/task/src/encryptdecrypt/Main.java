61987f11f3cd37bfaf22f1a1ae2cd0c1a807f346
mroui <martynaxroj@gmail.com>
2020-07-25 14:30:38 +0200

    [ADD] checking operation encrypt/decrypt/exit & add message & shift by std input

--- Encryption-Decryption/Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -1,12 +1,24 @@
 package encryptdecrypt;
 
 import java.util.Scanner;
+import static encryptdecrypt.EncryptionDecryption.*;
 
 public class Main {
 
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        System.out.println(Encryption.byShift(scanner.nextLine(), scanner.nextInt()));
-        scanner.close();
+        switch (scanner.nextLine()) {
+            case "enc":
+                System.out.println(byShift(scanner.nextLine(), scanner.nextInt(), true));
+                break;
+            case "dec":
+                System.out.println(byShift(scanner.nextLine(), scanner.nextInt(), false));
+                break;
+            case "exit":
+                scanner.close();
+                return;
+            default:
+                System.out.println("Unknown operation");
+        }
     }
 }
