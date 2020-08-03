7150786c3ab0ce629fd0fd333eb6fe495819ecdd
mroui <martynaxroj@gmail.com>
2020-08-03 19:07:49 +0200

    [MODIFY] make GameOfLife JFrame, move main & add board, labels about gen & alives & game loop

--- Game of Life/Game of Life/task/src/life/GameOfLife.java
+++ Game of Life/Game of Life/task/src/life/GameOfLife.java
@@ -1,43 +1,52 @@
 package life;
 
-import java.io.IOException;
-import java.util.Scanner;
+import javax.swing.*;
 
-public class GameOfLife {
+public class GameOfLife extends JFrame {
 
-    private final Universe universe;
-    private static int epoch;
+    private JLabel generationLabel;
+    private JLabel aliveLabel;
+    private Board board;
 
-    public GameOfLife(Universe universe) {
-        this.universe = universe;
-        epoch = 1;
+    public static void main(String[] args) {
+        GameOfLife game = new GameOfLife();
+        game.play();
     }
 
-    public static GameOfLife create() {
-        final Scanner scanner = new Scanner(System.in);
-        int size = scanner.nextInt();
-        scanner.close();
-        return new GameOfLife(new Universe(size));
+    public GameOfLife() {
+        setTitle("Game of Life");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 300);
+        setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+        setLocationRelativeTo(null);
+        setComponents();
+        setVisible(true);
     }
 
-    public void play() {
-        while(epoch <= 10) {
-            clear();
-            System.out.println("Generation: #" + epoch);
-            System.out.println("Alive: " + universe.nextGen().getAliveAmount());
-            universe.nextGen().print();
-            universe.generate();
-            epoch++;
+    private void play() {
+        while (board.getEpoch() <= 10) {
+            repaint();
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException ignored) {}
+            generationLabel.setText("Generation: #" + board.getEpoch());
+            aliveLabel.setText("Alive: " + board.getAlive());
+            board.generate();
+            board.nextEpoch();
         }
     }
 
-    public void clear() {
-        try {
-            if (System.getProperty("os.name").contains("Windows"))
-                new ProcessBuilder("cmd","/c","cls").inheritIO().start().waitFor();
-            else
-                Runtime.getRuntime().exec("clear");
-        }
-        catch (IOException | InterruptedException ignored) {}
+    private void setComponents() {
+        generationLabel = new JLabel("Generation #0");
+        generationLabel.setName("GenerationLabel");
+        add(generationLabel);
+
+        aliveLabel = new JLabel("Alive: 0");
+        aliveLabel.setName("AliveLabel");
+        add(aliveLabel);
+
+        board = new Board(new Universe(10));
+        board.setName("Board");
+        add(board);
     }
 }
--- Game of Life/Game of Life/task/src/life/Main.java
+++
@@ -1,8 +0,0 @@
-package life;
-
-public class Main {
-    public static void main(String[] args) {
-        GameOfLife game = GameOfLife.create();
-        game.play();
-    }
-}
