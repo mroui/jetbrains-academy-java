0c0cd3ade3155e94fdd099ebac3ef66e412acb93
mroui <martynaxroj@gmail.com>
2020-08-05 20:45:12 +0200

    [MODIFY] make board gray color

--- Game of Life/Game of Life/task/src/life/Board.java
+++ Game of Life/Game of Life/task/src/life/Board.java
@@ -42,7 +42,7 @@ public class Board extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
-        g2d.setColor(Color.BLACK);
+        g2d.setColor(Color.GRAY);
         int size = universe.nextGen().getSize();
         int cs = GameOfLife.CELL_SIZE;
         for (int i = 0; i < size; i++) {
