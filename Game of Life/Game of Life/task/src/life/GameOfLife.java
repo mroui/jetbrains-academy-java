676f7cf9a53e74cdc7777a57801f9a579cb6d7de
mroui <martynaxroj@gmail.com>
2020-08-05 20:46:07 +0200

    [MODIFY] implement timer with pausing, resuming, replaying & implement Const interface

--- Game of Life/Game of Life/task/src/life/GameOfLife.java
+++ Game of Life/Game of Life/task/src/life/GameOfLife.java
@@ -4,72 +4,126 @@ import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
-public class GameOfLife extends JFrame {
+public class GameOfLife extends JFrame implements Constants {
 
-    public static final int CELL_SIZE = 5;
-
+    private Timer timer;
+    private JPanel mainPanel;
+    private JPanel menuPanel;
     private JLabel generationLabel;
     private JLabel aliveLabel;
     private Board board;
+    private boolean paused;
 
     public static void main(String[] args) {
-        GameOfLife game = new GameOfLife();
-        game.play();
+        new GameOfLife();
     }
 
     public GameOfLife() {
+        paused = false;
         setTitle("Game of Life");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(800, 600);
+        setSize(WINDOW_W, WINDOW_H);
         setLayout(new BorderLayout());
         setLocationRelativeTo(null);
         setComponents();
         setResizable(false);
         setVisible(true);
+        play();
     }
 
     private void play() {
-        while (board.getEpoch() <= 10) {
+        timer = new Timer(TIMER_DELAY, evt -> {
             repaint();
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException ignored) {}
-            generationLabel.setText("Generation: #" + board.getEpoch());
-            aliveLabel.setText("Alive: " + board.getAlive());
+            generationLabel.setText(GEN + board.getEpoch());
+            aliveLabel.setText(ALIVE + board.getAlive());
             board.generate();
             board.nextEpoch();
-        }
+        });
+        timer.start();
     }
 
     private void setComponents() {
-        JPanel mainPanel = new JPanel();
+        mainPanel = new JPanel();
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.X_AXIS));
-        mainPanel.setBorder(new EmptyBorder(new Insets(20, 20, 20, 20)));
+        mainPanel.setBorder(EMPTY_BORDER);
         add(mainPanel);
 
-        JPanel menuPanel = new JPanel();
+        menuPanel = new JPanel();
         menuPanel.setLayout(new BoxLayout(menuPanel, BoxLayout.Y_AXIS));
-        menuPanel.setPreferredSize(new Dimension((int) (getWidth() * 0.25), getHeight()));
-        menuPanel.setMaximumSize(menuPanel.getPreferredSize());
+        menuPanel.setPreferredSize(MENU_DIM);
+        menuPanel.setMaximumSize(MENU_DIM);
         mainPanel.add(menuPanel);
-        addMenu(menuPanel);
+        addMenu();
 
-        board = new Board(new Universe((int) (getWidth() * 0.75 / CELL_SIZE - 12)));
-        board.setName("Board");
+        createBoard();
+    }
+
+    private void createBoard() {
+        Component existedBoard = getComponentByName(BOARD_NAME, mainPanel.getComponents());
+        if (existedBoard != null)
+            mainPanel.remove(existedBoard);
+
+        board = new Board(new Universe(BOARD_SIZE));
+        board.setName(BOARD_NAME);
         mainPanel.add(board);
     }
 
-    private void addMenu(JPanel panel) {
-        Font font = new Font(Font.MONOSPACED,  Font.BOLD, 14);
+    private void addMenu() {
+        addButtons();
+        addStatistics();
+    }
 
-        generationLabel = new JLabel("Generation #0");
-        generationLabel.setName("GenerationLabel");
-        generationLabel.setFont(font);
-        panel.add(generationLabel);
+    private void addButtons() {
+        JPanel buttonsPanel = new JPanel();
+        buttonsPanel.setPreferredSize(BUTTONS_PANEL_DIM);
+        buttonsPanel.setMaximumSize(BUTTONS_PANEL_DIM);
 
-        aliveLabel = new JLabel("Alive: 0");
-        aliveLabel.setName("AliveLabel");
-        aliveLabel.setFont(font);
-        panel.add(aliveLabel);
+        JToggleButton pauseResumeButton = new JToggleButton(PAUSE_ICON);
+        pauseResumeButton.setName(PAUSE_RESUME_NAME);
+        pauseResumeButton.setPreferredSize(BUTTON_DIM);
+        pauseResumeButton.addActionListener(e -> {
+            pauseResumeButton.setIcon(!paused ? RESUME_ICON : PAUSE_ICON);
+            paused = !paused;
+            if (paused) timer.stop();
+            else timer.restart();
+        });
+        buttonsPanel.add(pauseResumeButton);
+
+        JButton replayButton = new JButton(REPLAY_ICON);
+        replayButton.setName(REPLAY_NAME);
+        replayButton.setPreferredSize(BUTTON_DIM);
+        replayButton.addActionListener(e -> {
+            createBoard();
+            if (paused) pauseResumeButton.doClick();
+            timer.start();
+        });
+        buttonsPanel.add(replayButton);
+
+        menuPanel.add(buttonsPanel);
+    }
+
+    private void addStatistics() {
+        JPanel statisticsPanel = new JPanel();
+
+        statisticsPanel.setLayout(new FlowLayout(FlowLayout.CENTER, MENU_GAP_H, MENU_GAP_W));
+        generationLabel = new JLabel(GEN + '0');
+        generationLabel.setName(GEN_LABEL_NAME);
+        generationLabel.setFont(FONT);
+        statisticsPanel.add(generationLabel);
+
+        aliveLabel = new JLabel(ALIVE + '0');
+        aliveLabel.setName(ALIVE_LABEL_NAME);
+        aliveLabel.setFont(FONT);
+        statisticsPanel.add(aliveLabel);
+
+        menuPanel.add(statisticsPanel);
+    }
+
+    public Component getComponentByName(String name, Component[] list) {
+        for (Component component : list) {
+            if (component.getName() != null && component.getName().equals(name))
+                return component;
+        }
+        return null;
     }
 }
