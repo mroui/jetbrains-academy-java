6ed8a8e0e7cf3317677126fc8555e20476c682fa
mroui <martynaxroj@gmail.com>
2020-08-07 22:33:43 +0200

    [ADD] Constants interface with basic dimensions & objects

---
+++ Web Crawler/Web Crawler/task/src/crawler/Constants.java
@@ -0,0 +1,17 @@
+package crawler;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+
+public interface Constants {
+
+    int WINDOW_SIZE = 300;
+
+    String APP_TITLE = "Simple Window";
+    String HTML_AREA_TXT = "HTML code";
+    String TEXT_AREA = "TextArea";
+
+    Border SMALL_BORDER = BorderFactory.createEmptyBorder(10, 10, 10, 10);
+    Border NORMAL_BORDER = BorderFactory.createEmptyBorder(20, 20, 20, 20);
+
+}
