d962e3e960e2ce2d94fafef715d5c4bbe6afc0a8
mroui <martynaxroj@gmail.com>
2020-08-07 23:24:42 +0200

    [ADD] set url & html areas objects as class fields & set run button action with downloading url site text

--- Web Crawler/Web Crawler/task/src/crawler/app/WebCrawler.java
+++ Web Crawler/Web Crawler/task/src/crawler/app/WebCrawler.java
@@ -1,13 +1,22 @@
 package crawler.app;
 
+import crawler.utils.Constants;
+
 import javax.swing.*;
 import java.awt.*;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 import static crawler.utils.Constants.*;
 
 public class WebCrawler extends JFrame {
 
     private JPanel mainPanel;
+    private URLArea urlArea;
+    private HtmlArea htmlArea;
 
     public WebCrawler() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -21,13 +30,31 @@ public class WebCrawler extends JFrame {
 
     private void setComponents() {
         setPanel();
-        mainPanel.add(new URLArea(), BorderLayout.NORTH);
-        mainPanel.add(new HtmlArea(), BorderLayout.CENTER);
+        setAreas();
+        setAction();
+    }
+
+    private void setAreas() {
+        urlArea = new URLArea();
+        mainPanel.add(urlArea, BorderLayout.NORTH);
+        htmlArea = new HtmlArea();
+        mainPanel.add(htmlArea, BorderLayout.CENTER);
     }
 
     private void setPanel() {
         mainPanel = new JPanel(new BorderLayout(GAP_SIZE, GAP_SIZE));
         mainPanel.setBorder(NORMAL_BORDER);
         add(mainPanel);
+    }
+
+    private void setAction() {
+        urlArea.getRunButton().addActionListener(e -> {
+            final String url = urlArea.getUrlTextField().getText();
+            try (InputStream inputStream = new BufferedInputStream(new URL(url).openStream())) {
+                htmlArea.setText(new String(inputStream.readAllBytes(), StandardCharsets.UTF_8));
+            } catch (IOException exception) {
+                htmlArea.setText(Constants.ERROR + exception.getLocalizedMessage());
+            }
+        });
     }
 }
