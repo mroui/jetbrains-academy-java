3d1ab57d6542506d7c7dc5c9b5ec6aad888e8c94
mroui <martynaxroj@gmail.com>
2020-08-20 11:50:06 +0200

    [ADD] 20.08 daily task with hour of day by given seconds

---
+++ Web Crawler/Problems/Seconds since start of a day/src/Main.java
@@ -0,0 +1,8 @@
+import java.time.LocalTime;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println(LocalTime.ofSecondOfDay(new Scanner(System.in).nextInt()));
+    }
+}
