a9268c7b10d86baf31f9c423baa8ecdd3be4101b
mroui <martynaxroj@gmail.com>
2020-08-25 17:19:58 +0200

    [ADD] recognition 0 / 1 by Perceptron using Grid

--- Digit Recognition/Digit Recognition/task/src/recognition/Main.java
+++ Digit Recognition/Digit Recognition/task/src/recognition/Main.java
@@ -1,8 +1,7 @@
 package recognition;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        System.out.println("This number is " + Perceptron.recognise01(Grid.read(3, 3)));
     }
 }
