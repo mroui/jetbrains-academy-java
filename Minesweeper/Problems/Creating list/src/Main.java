eca7e09a6d1cf01211b36b9252dda7ff989fc901
mroui <martynaxroj@gmail.com>
2020-09-15 17:19:57 +0200

    [ADD] simple tasks with arraylist

---
+++ Minesweeper/Problems/Creating list/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(2, 0, 1, 7));
+        System.out.println(list);
+    }
+}
