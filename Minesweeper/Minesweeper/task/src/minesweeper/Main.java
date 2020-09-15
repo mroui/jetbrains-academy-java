895d729ade142f60a84af9cb0f6f8cac3fbe1f0b
mroui <martynaxroj@gmail.com>
2020-09-15 15:46:25 +0200

    [ADD] task with printing random board with mines

--- Minesweeper/Minesweeper/task/src/minesweeper/Main.java
+++ Minesweeper/Minesweeper/task/src/minesweeper/Main.java
@@ -2,6 +2,8 @@ package minesweeper;
 
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        GameBoard board = new GameBoard(3, 3, new boolean[][]{{true, false, false}, {false,false, true}, {false, false, true}});
+        board.addMines(3);
+        board.print();
     }
 }
