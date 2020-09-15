4b2ad18123e97ebb7e79290950c34414376e7e2a
mroui <martynaxroj@gmail.com>
2020-09-15 12:45:43 +0200

    [FIX] menu loop & new lines

--- Digit Recognition/Digit Recognition/task/src/recognition/Main.java
+++ Digit Recognition/Digit Recognition/task/src/recognition/Main.java
@@ -6,21 +6,21 @@ public class Main {
     public static void main(String[] args) {
         boolean isMenu = true;
         while (isMenu) {
-            System.out.println("1. Learn the network\n2. Guess a number\nYour choice: ");
+            System.out.print("1. Learn the network\n2. Guess a number\nYour choice: ");
             int option = Integer.parseInt(new Scanner(System.in).nextLine());
             switch (option) {
                 case 1:
                     System.out.println("Learning...");
                     NeuralNetwork.learn(10);
                     NeuralNetwork.saveToFile();
-                    System.out.println("Done! Saved to the file.");
-                    return;
+                    System.out.println("Done! Saved to the file.\n");
+                    break;
                 case 2:
                     NeuralNetwork.learn(10);
                     System.out.println("Input grid:");
                     Grid grid = Grid.read(5, 3);
-                    System.out.println("This number is " + NeuralNetwork.recognise09(grid));
-                    return;
+                    System.out.println("This number is " + NeuralNetwork.recognise09(grid) + "\n");
+                    break;
                 default:
                     isMenu = false;
             }
