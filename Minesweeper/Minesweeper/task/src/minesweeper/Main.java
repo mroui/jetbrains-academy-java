7996682ddd3ee2760ac8c9b86fb88f0f7cabe20f
mroui <martynaxroj@gmail.com>
2020-09-17 14:38:11 +0200

    [ADD] task with checking mines neighbours & add test for 3/5 stage

--- Minesweeper/Minesweeper/task/src/minesweeper/Main.java
+++ Minesweeper/Minesweeper/task/src/minesweeper/Main.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         GameBoard board = new GameBoard(9, 9);
-        System.out.println("How many mines do you want on the field?");
+        System.out.print("How many mines do you want on the field? ");
         int mines = new Scanner(System.in).nextInt();
         board.addMines(mines);
+        board.checkMines();
         board.print();
     }
 }
