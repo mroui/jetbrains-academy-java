644c53651d3bab519ed1c80008ea2d8fc4394d90
mroui <martynaxroj@gmail.com>
2020-09-17 14:37:26 +0200

    [ADD] checking mines - counting neighbours & helping methods like is cell empty

--- Minesweeper/Minesweeper/task/src/minesweeper/GameBoard.java
+++ Minesweeper/Minesweeper/task/src/minesweeper/GameBoard.java
@@ -10,20 +10,48 @@ public class GameBoard extends Board {
         super(rows, columns, array);
     }
 
+    private void setMine(int x, int y) {
+        get()[x][y] = 'X';
+    }
+
     public void addMines(int amount) {
         if (amount < empties()) {
             for (int i = 0; i < amount; i++) {
                 int x = rand(0, rows() - 1);
                 int y = rand(0, cols() - 1);
-                if (!get()[x][y])
-                    get()[x][y] = true;
+                if (!isMineOrExist(x, y))
+                    setMine(x, y);
                 else i--;
             }
         } else {
             for (int i = 0; i < rows(); i++)
                 for (int j = 0; j < cols(); j++)
-                    get()[i][j] = true;
+                    setMine(i, j);
         }
+    }
+
+    public void checkMines() {
+        for (int i = 0; i < rows(); i++) {
+            for (int j = 0; j < cols(); j++) {
+                if (!isMineOrExist(i, j)) {
+                    int counter = 0;
+                    for (int x = i - 1; x <= i + 1; x++)
+                        for (int y = j - 1; y <= j + 1; y++)
+                            if ((x != i || y != j) && isMineOrExist(x, y))
+                                counter++;
+                    if (counter > 0)
+                        get()[i][j] = isEmpty(i, j) ? (char) (counter + '0') : (char) ((int) (get()[i][j]) + counter);
+                }
+            }
+        }
+    }
+
+    private boolean isMineOrExist(int x, int y) {
+        return x >= 0 && x < rows() && y >= 0 && y < cols() && get()[x][y] == 'X';
+    }
+
+    private boolean isEmpty(int x, int y) {
+        return get()[x][y] == '.';
     }
 
     private int rand(int min, int max) {
