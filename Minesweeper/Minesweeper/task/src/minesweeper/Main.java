de1341b13a44a3c4004de31fc5eb27c5193a7a2a
mroui <martynaxroj@gmail.com>
2020-09-17 16:32:46 +0200

    [ADD] play game for 4/5 stage & add tests

--- Minesweeper/Minesweeper/task/src/minesweeper/Main.java
+++ Minesweeper/Minesweeper/task/src/minesweeper/Main.java
@@ -1,14 +1,7 @@
 package minesweeper;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        GameBoard board = new GameBoard(9, 9);
-        System.out.print("How many mines do you want on the field? ");
-        int mines = new Scanner(System.in).nextInt();
-        board.addMines(mines);
-        board.checkMines();
-        board.print();
+        new Game(9, 9).play();
     }
 }
