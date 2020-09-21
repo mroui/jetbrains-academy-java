fd1ab1f8869fefe1b3053c097ea4ce0ba2b23bef
mroui <martynaxroj@gmail.com>
2020-09-21 19:01:54 +0200

    [ADD] variables support

--- Smart Calculator/Smart Calculator/task/src/calculator/Main.java
+++ Smart Calculator/Smart Calculator/task/src/calculator/Main.java
@@ -1,11 +1,13 @@
 package calculator;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        Map<String, Integer> variables = new HashMap<>();
         Scanner scanner = new Scanner(System.in);
         boolean running = true;
         while (running) {
@@ -24,16 +26,38 @@ public class Main {
                             System.out.println("Unknown command");
                     }
                 } else {
-                    String[] parts = input.trim().split("\\s+");
-                    int result = 0, sign = 1;
-                    for (String part : parts) {
-                        if(part.matches("[-+]+")){
-                            sign = part.contains("+") ? 1 : part.contains("-") ? part.length() % 2 == 0 ? 1 : -1 : sign;
+                    // a = 5, a=5, a       = 5, c = a
+                    if (input.replaceAll("\\s+", "").matches("[A-Za-z]+[A-Za-z0-9]?+=[A-Za-z0-9]+")) {
+                        String[] strings = input.replaceAll("\\s+", "").split("=");
+                        if (strings[0].matches(".*[0-9]")) {
+                            System.out.println("Invalid identifier");
+                        } else if (strings[1].matches("\\D") && variables.get(strings[1]) == null) {
+                            System.out.println("Unknown variable");
+                        } else if (strings[1].matches("[A-Za-z]+") && variables.get(strings[1]) != null) {
+                            variables.put(strings[0], variables.get(strings[1]));
+                        } else if (strings[1].matches("\\d+")) {
+                            variables.put(strings[0], Integer.parseInt(strings[1]));
                         } else {
-                            result += sign * Integer.parseInt(part);
+                            System.out.println("Invalid assignment");
                         }
-                    }
-                    System.out.println(result);
+                        //a
+                    } else if (input.replaceAll("\\s+", "").matches("[A-Za-z0-9]+")) {
+                        System.out.println(variables.get(input) != null ? variables.get(input) : "Unknown variable");
+                        //1 + 5 - a, ---, ++, b - a
+                    } else if (input.replaceAll("\\s+", "").matches("[A-Za-z0-9]+[-+]+[A-Za-z0-9]+.*")) {
+                        String[] parts = input.trim().split("\\s+");
+                        int result = 0, sign = 1;
+                        for (String part : parts) {
+                            if (part.matches("[-+]+")) {
+                                sign = part.contains("+") ? 1 : part.contains("-") ? part.length() % 2 == 0 ? 1 : -1 : sign;
+                            } else if (part.matches("-?[0-9]+")) {
+                                result += sign * Integer.parseInt(part);
+                            } else if (variables.containsKey(part)) {
+                                result += sign * variables.get(part);
+                            }
+                        }
+                        System.out.println(result);
+                    } else System.out.println("Invalid identifier");
                 }
             } catch (Exception e) {
                 System.out.println("Invalid expression");
