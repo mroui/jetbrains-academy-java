f9e25e45bb2fb26b12e7e5d48d3b5d44b326dd4e
mroui <martynaxroj@gmail.com>
2020-09-21 15:53:21 +0200

    [ADD] reading numbers in loop & sum by stream, end by /exit & by empty string continue

--- Smart Calculator/Smart Calculator/task/src/calculator/Main.java
+++ Smart Calculator/Smart Calculator/task/src/calculator/Main.java
@@ -1,18 +1,23 @@
 package calculator;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        try {
-            String[] numbers = scanner.nextLine().split("\\s+");
-            int x = Integer.parseInt(numbers[0]);
-            int y = Integer.parseInt(numbers[1]);
-            System.out.println(x + y);
-        } catch (Exception e) {
-            System.out.println("Error: " + e + " -> input should looks like: [number] [number]");
+        while (true) {
+            try {
+                String input = scanner.nextLine();
+                if (input.isEmpty()) continue;
+                if ("/exit".equals(input)) break;
+                int[] numbers = Arrays.stream(input.trim().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+                System.out.println(Arrays.stream(numbers).sum());
+            } catch (Exception e) {
+                System.out.println("Error: " + e + " -> input should looks like: [number] [number]\nTo exit -> /exit");
+            }
         }
+        System.out.println("Bye!");
     }
 }
