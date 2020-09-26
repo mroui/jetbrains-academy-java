573ff8587c55170b840bfe8125634993e2a1a1bb
mroui <martynaxroj@gmail.com>
2020-09-26 17:57:16 +0200

    [ADD] solving linear equation by a & b in input

--- Linear Equations Solver/Linear Equations Solver/task/src/solver/Main.java
+++ Linear Equations Solver/Linear Equations Solver/task/src/solver/Main.java
@@ -1,7 +1,20 @@
 package solver;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        try {
+            double a = Double.parseDouble(scanner.next());
+            double b = Double.parseDouble(scanner.next());
+            System.out.println(linearEquation(a, b));
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
+
+    private static double linearEquation(double a, double b) {
+        return b / a;
     }
 }
