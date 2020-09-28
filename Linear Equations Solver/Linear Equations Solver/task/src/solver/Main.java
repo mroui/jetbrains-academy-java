d1420cd257c9cdd8badf9bc6fe13a7d38b30e6eb
mroui <martynaxroj@gmail.com>
2020-09-28 14:22:30 +0200

    [MODIFY] parsing arguments with filenames

--- Linear Equations Solver/Linear Equations Solver/task/src/solver/Main.java
+++ Linear Equations Solver/Linear Equations Solver/task/src/solver/Main.java
@@ -1,11 +1,25 @@
 package solver;
 
 public class Main {
+
     public static void main(String[] args) {
-        Equation e1 = Equation.read();
-        Equation e2 = Equation.read();
-        if (e1 != null && e2 != null) {
-            e1.calculate(e2);
+        run(args);
+    }
+
+    private static void run(String[] args) {
+        for (int i = 0; i < args.length; i += 2) {
+            switch (args[i]) {
+                case "-in":
+                    //todo
+                    break;
+                case "-out":
+                    //todo
+                    break;
+                default:
+                    System.out.println("Unknown argument: " + args[i]);
+                    break;
+            }
         }
     }
+
 }
