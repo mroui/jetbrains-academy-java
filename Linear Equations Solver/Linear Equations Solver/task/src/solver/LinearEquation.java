568517e5eeebfada5f6bb044b7d0eef719ae164d
mroui <martynaxroj@gmail.com>
2020-09-28 14:24:11 +0200

    [MODIFY] linear equation with multiple coefficients

---
+++ Linear Equations Solver/Linear Equations Solver/task/src/solver/LinearEquation.java
@@ -0,0 +1,49 @@
+package solver;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class LinearEquation {
+
+    private final String INSUFFICIENT_COE = "Insufficient number of coefficients";
+    private final double[] coefficients;
+
+    public LinearEquation(double ... c) {
+        coefficients = c.clone();
+    }
+
+    private double c(int i) {
+        return coefficients[i];
+    }
+
+    public static LinearEquation read(int coeAmount) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            List<Double> coefficients = new ArrayList<>();
+            while (coeAmount > 0) {
+                coefficients.add(Double.parseDouble(scanner.next()));
+                coeAmount--;
+            }
+            return new LinearEquation(coefficients.stream().mapToDouble(Double::doubleValue).toArray());
+        } catch (Exception e) {
+            out(e.toString());
+        }
+        return null;
+    }
+
+    public void calculate() {
+        out(coefficients.length > 1 ? coefficients[1] / coefficients[0] : INSUFFICIENT_COE);
+    }
+
+    public void calculateSystem(LinearEquation e) {
+        if (coefficients.length > 2 && e.coefficients.length > 2) {
+            double y = (e.c(2) - c(2) * e.c(0) / c(0)) / (e.c(1) - c(1) * e.c(0) / c(0));
+            double x = (c(2) - c(1) * y) / c(0);
+            out(x + " " + y);
+        } else out(INSUFFICIENT_COE);
+    }
+
+    private static void out(Object msg) {
+        System.out.println(msg);
+    }
+}
