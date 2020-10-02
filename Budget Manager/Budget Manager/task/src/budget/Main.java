58dae42e5f90d905ca0974aeea5ca26592a51f58
mroui <martynaxroj@gmail.com>
2020-10-02 17:10:30 +0200

    [ADD] finish stage 2 with menu - income, purchases & balance

--- Budget Manager/Budget Manager/task/src/budget/Main.java
+++ Budget Manager/Budget Manager/task/src/budget/Main.java
@@ -1,22 +1,7 @@
 package budget;
 
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        double sum = 0;
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            System.out.println(line);
-            Matcher matcher = Pattern.compile("\\$(\\d+\\.\\d+|\\d+)").matcher(line);
-            if (matcher.find()) {
-                String value = matcher.group(0);
-                sum += Double.parseDouble(value.substring(1));
-            }
-        }
-        System.out.println("\nTotal: $" + sum);
+        new BudgetManager().run();
     }
 }
