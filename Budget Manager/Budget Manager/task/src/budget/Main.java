18b3a5cb68eff85257cbb55f43045383ba94c3e4
mroui <martynaxroj@gmail.com>
2020-10-02 14:39:30 +0200

    [ADD] searching for expenses, decimal regex & sum total value

--- Budget Manager/Budget Manager/task/src/budget/Main.java
+++ Budget Manager/Budget Manager/task/src/budget/Main.java
@@ -1,7 +1,22 @@
 package budget;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Scanner scanner = new Scanner(System.in);
+        double sum = 0;
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            System.out.println(line);
+            Matcher matcher = Pattern.compile("\\$(\\d+\\.\\d+|\\d+)").matcher(line);
+            if (matcher.find()) {
+                String value = matcher.group(0);
+                sum += Double.parseDouble(value.substring(1));
+            }
+        }
+        System.out.println("\nTotal: $" + sum);
     }
 }
