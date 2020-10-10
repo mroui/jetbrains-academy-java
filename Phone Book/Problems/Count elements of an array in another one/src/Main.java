54555a356ce38ecda2dbffb9be7dd632f5b638cb
mroui <martynaxroj@gmail.com>
2020-10-10 11:02:12 +0200

    [ADD] counting occurences from one in second array

---
+++ Phone Book/Problems/Count elements of an array in another one/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static int[] countOccurrences(int[] first, int[] second) {
+        int[] result = new int[first.length];
+        for (int i = 0; i < first.length; i++) {
+            for (int k : second) {
+                if (first[i] == k) {
+                    result[i]++;
+                }
+            }
+        }
+        return result;
+    }
+
+    /* Do not change code below */
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        final int[] first = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        final int[] second = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        final String result = Arrays.toString(countOccurrences(first, second))
+                .replace(", ", " ")
+                .replace("[", "")
+                .replace("]", "");
+        System.out.println(result);
+    }
+}
