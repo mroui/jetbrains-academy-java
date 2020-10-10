da70b013691a12c7b6314bde54e106121184b662
mroui <martynaxroj@gmail.com>
2020-10-10 16:44:03 +0200

    [ADD] call phone book & linear search, add test & finish stage 1

--- Phone Book/Phone Book/task/src/phonebook/Main.java
+++ Phone Book/Phone Book/task/src/phonebook/Main.java
@@ -2,5 +2,7 @@ package phonebook;
 
 public class Main {
     public static void main(String[] args) {
+        PhoneBook phoneBook = new PhoneBook(PhoneBook.read("files/directory.txt"));
+        phoneBook.search(PhoneBook.read("files/find.txt"));
     }
 }
