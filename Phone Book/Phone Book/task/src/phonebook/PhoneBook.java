7eb04379d90e0a4e0eb440457ba21b38eaf16486
mroui <martynaxroj@gmail.com>
2020-10-12 21:38:55 +0200

    [ADD] jump and backward search & run linear & jump searching with bubble sort and counting time

--- Phone Book/Phone Book/task/src/phonebook/PhoneBook.java
+++ Phone Book/Phone Book/task/src/phonebook/PhoneBook.java
@@ -3,17 +3,21 @@ package phonebook;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PhoneBook {
 
     private final List<Person> records;
+    private final Sort<Person> sort;
+    private final Search<Person> search;
 
     public PhoneBook(List<Person> list) {
         records = new ArrayList<>(list);
+        sort = new Sort<>();
+        search = new Search<>();
     }
 
     public static List<Person> read(String filename) {
@@ -40,14 +44,47 @@ public class PhoneBook {
     }
 
     public void search(List<Person> list) {
-        System.out.println("Start searching...");
+        long time = linearSearch(list);
+        jumpSearch(list, time * 200);
+    }
+
+    private long linearSearch(List<Person> list) {
+        System.out.println("Start searching (linear search)...");
         long time = System.currentTimeMillis();
-        long founded = Search.linear(Arrays.asList(records.toArray()), Arrays.asList(list.toArray()));
-        time = System.currentTimeMillis() - time;
+        long founded = search.linear(records, list);
+        System.out.println("Found " + founded + " / " + list.size() +
+                " entries. Time taken: " + getTimeTakenString(time) + '\n');
+        return System.currentTimeMillis() - time;
+    }
+
+    private void jumpSearch(List<Person> list, long maxTime) {
+        System.out.println("Start searching (bubble sort + jump search)...");
+        long allTime = System.currentTimeMillis();
+        boolean done;
+        Future<Boolean> future = Executors.newCachedThreadPool().submit(() -> {
+            sort.bubble(records);
+            return true;
+        });
+        try {
+            done = future.get(maxTime, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            done = false;
+        }
+        String sortingTimeString = getTimeTakenString(allTime);
+        long searchingTime = System.currentTimeMillis();
+        long founded = done ? search.jump(records, list) : search.linear(records, list);
+        String searchingTimeString = getTimeTakenString(searchingTime);
+        System.out.println("Found " + founded + " / " + list.size() + " entries. " +
+                "Time taken: " + getTimeTakenString(allTime));
+        System.out.println("Sorting time: " + sortingTimeString + (done ? "" : " - STOPPED, moved to linear search"));
+        System.out.println("Searching time: " + searchingTimeString + '\n');
+    }
+
+    private String getTimeTakenString(long startTime) {
+        long time = System.currentTimeMillis() - startTime;
         long minutes = time / 60000;
         long seconds = time % 60000 / 1000;
         long milliseconds = (time % 60000) % 1000;
-        System.out.println("Found " + founded + " / " + list.size() + " entries. Time taken: " + minutes + " min. " +
-                seconds + " sec. " + milliseconds + " ms.");
+        return minutes + " min. " + seconds + " sec. " + milliseconds + " ms.";
     }
 }
