c79d8233b1b38573f6918a68c52cf9c6d7e87769
mroui <martynaxroj@gmail.com>
2020-10-13 16:35:47 +0200

    [ADD] quicksort with partitioning list & swapping elements methods

--- Phone Book/Phone Book/task/src/phonebook/Sort.java
+++ Phone Book/Phone Book/task/src/phonebook/Sort.java
@@ -13,4 +13,30 @@ public class Sort<T extends Comparable<T>> {
                     source.set(j + 1, temp);
                 }
     }
+
+    public void quick(List<T> source, int left, int right) {
+        if (left < right) {
+            int pivotIndex = partition(source, left, right);
+            quick(source, left, pivotIndex - 1);
+            quick(source, pivotIndex + 1, right);
+        }
+    }
+    private int partition(List<T> source, int left, int right) {
+        T pivot = source.get(right);
+        int partitionIndex = left;
+        for (int i = left; i < right; i++) {
+            if (source.get(i).compareTo(pivot) <= 0) {
+                swap(source, i, partitionIndex);
+                partitionIndex++;
+            }
+        }
+        swap(source, partitionIndex, right);
+        return partitionIndex;
+    }
+
+    private void swap(List<T> source, int prev, int next) {
+        T temp = source.get(prev);
+        source.set(prev, source.get(next));
+        source.set(next, temp);
+    }
 }
