329848163a01684cdf12767ee286b5b45e97d8c3
mroui <martynaxroj@gmail.com>
2020-10-26 14:17:12 +0100

    [MODIFY] item category value to int & add get method with string double parsing

--- Budget Manager/Budget Manager/task/src/budget/item/ItemCategory.java
+++ Budget Manager/Budget Manager/task/src/budget/item/ItemCategory.java
@@ -2,19 +2,27 @@ package budget.item;
 
 public enum ItemCategory {
 
-    FOOD("1"), CLOTHES("2"), ENTERTAINMENT("3"), OTHER("4");
+    FOOD(1), CLOTHES(2), ENTERTAINMENT(3), OTHER(4);
 
-    final String value;
+    private final int value;
 
-    ItemCategory(String value) {
+    ItemCategory(int value) {
         this.value = value;
     }
 
-    public static ItemCategory get(String value) {
+    public static ItemCategory get(int value) {
         for (ItemCategory i : values())
-            if (i.value.equals(value))
+            if (i.value == value)
                 return i;
         return null;
+    }
+
+    public static ItemCategory get(String value) {
+        try {
+            return get(Integer.parseInt(value));
+        } catch (NumberFormatException e) {
+            return null;
+        }
     }
 
     @Override
