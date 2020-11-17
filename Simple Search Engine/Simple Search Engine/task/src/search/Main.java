8ee719674b29d1df569a5d643015b75867d7a3dc
mroui <martynaxroj@gmail.com>
2020-11-17 23:19:54 +0100

    [ADD] stage1 - finding string index in text

--- Simple Search Engine/Simple Search Engine/task/src/search/Main.java
+++ Simple Search Engine/Simple Search Engine/task/src/search/Main.java
@@ -1,7 +1,18 @@
 package search;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        List<String> baseWords = readWords();
+        List<String> wordsToFind = readWords();
+        for (String word : wordsToFind)
+            System.out.print((baseWords.contains(word) ? baseWords.indexOf(word) + 1 : "Not found") + " ");
+    }
+
+    private static List<String> readWords() {
+        return Arrays.asList(new Scanner(System.in).nextLine().split("\\s"));
     }
 }
