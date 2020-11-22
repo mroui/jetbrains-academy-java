d61941ac8e04b14bd210b713ef87472b0a388479
mroui <martynaxroj@gmail.com>
2020-11-22 19:46:40 +0100

    [ADD] InputReader class for all scanner stuff

---
+++ Simple Search Engine/Simple Search Engine/task/src/search/InputReader.java
@@ -0,0 +1,16 @@
+package search;
+
+import java.util.Scanner;
+
+public class InputReader {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static String readLine() {
+        return scanner.nextLine();
+    }
+
+    public static int readInt() {
+        return Integer.parseInt(readLine());
+    }
+}
