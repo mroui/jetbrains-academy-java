031520a6a421944758076b496568fa909615c6ca
mroui <martynaxroj@gmail.com>
2020-11-22 19:09:57 +0100

    [ADD] reading personal data & searching common data among people

--- Simple Search Engine/Simple Search Engine/task/src/search/Main.java
+++ Simple Search Engine/Simple Search Engine/task/src/search/Main.java
@@ -1,18 +1,55 @@
 package search;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+
+    static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        List<String> baseWords = readWords();
-        List<String> wordsToFind = readWords();
-        for (String word : wordsToFind)
-            System.out.print((baseWords.contains(word) ? baseWords.indexOf(word) + 1 : "Not found") + " ");
+        try {
+            searchPeople(readPeople());
+        } catch (Exception e) {
+            System.out.println("ERROR: " + e.toString());
+        }
     }
 
-    private static List<String> readWords() {
-        return Arrays.asList(new Scanner(System.in).nextLine().split("\\s"));
+    private static void searchPeople(List<Person> list) {
+        List<Person> foundPeople = new ArrayList<>();
+        System.out.println("\nEnter the number of search queries:");
+        int amount = Integer.parseInt(scanner.nextLine());
+        while (amount > 0) {
+            System.out.println("\nEnter data to search people:");
+            String data = scanner.nextLine();
+            list.forEach(person -> {
+                if (person.hasInCommon(data))
+                    foundPeople.add(person);
+            });
+            if (foundPeople.size() > 0) {
+                System.out.println("\nFound people:");
+                foundPeople.forEach(System.out::println);
+            } else System.out.println("No matching people found.");
+            amount--;
+            foundPeople.clear();
+        }
+    }
+
+    private static List<Person> readPeople() {
+        List<Person> people = new ArrayList<>();
+        System.out.println("Enter the number of people:");
+        int amount = Integer.parseInt(scanner.nextLine());
+        System.out.println("Enter all people:");
+        while (amount > 0) {
+            String[] personalData = readWords();
+            people.add(new Person(personalData[0], personalData[1], personalData.length == 3 ? personalData[2] : ""));
+            amount--;
+        }
+        return people;
+    }
+
+    private static String[] readWords() {
+        return scanner.nextLine().split("\\s");
     }
 }
