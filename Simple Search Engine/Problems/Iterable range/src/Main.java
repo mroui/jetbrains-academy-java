c88932c65e062c23c6bb8d4efea6851705d93e31
mroui <martynaxroj@gmail.com>
2020-11-30 19:44:09 +0100

    [ADD] 3 tasks with iterable interface

---
+++ Simple Search Engine/Problems/Iterable range/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Iterator;
+
+class Range implements Iterable<Long> {
+
+    private long fromInclusive;
+    private long toExclusive;
+
+    public Range(long from, long to) {
+        this.fromInclusive = from;
+        this.toExclusive = to;
+    }
+
+    @Override
+    public Iterator<Long> iterator() {
+        return new Iterator<>() {
+            @Override
+            public boolean hasNext() {
+                return fromInclusive < toExclusive;
+            }
+
+            @Override
+            public Long next() {
+                fromInclusive++;
+                return fromInclusive - 1;
+            }
+        };
+    }
+}
