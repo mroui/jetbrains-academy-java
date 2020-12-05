9a6616916d3caa1b5e635bb6a8b33321cca5cbff
mroui <martynaxroj@gmail.com>
2020-12-05 19:06:59 +0100

    [ADD] read numbers til eof & get max with amount of max task

--- Sorting Tool/Sorting Tool/task/src/sorting/Main.java
+++ Sorting Tool/Sorting Tool/task/src/sorting/Main.java
@@ -1,14 +1,20 @@
 package sorting;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(final String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        List<Long> numbers = new ArrayList<>();
         while (scanner.hasNextLong()) {
-            long number = scanner.nextLong();
-            // write your code here
+            numbers.add(scanner.nextLong());
         }
+        long size = numbers.size();
+        long greatest = numbers.stream().max(Long::compareTo).orElse(0L);
+        long greatestTimes = numbers.stream().filter(n -> n == greatest).count();
+        System.out.println("Total numbers: " + size + '.');
+        System.out.println("The largest number: " + greatest + " (" + greatestTimes + " time(s)).");
     }
 }
