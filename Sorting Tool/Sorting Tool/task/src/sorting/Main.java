37066a22a33bededc649ae967a97a9008392b13b
mroui <martynaxroj@gmail.com>
2020-12-05 19:55:38 +0100

    [ADD] reading also words/lines & calculate percentage of max element

--- Sorting Tool/Sorting Tool/task/src/sorting/Main.java
+++ Sorting Tool/Sorting Tool/task/src/sorting/Main.java
@@ -1,20 +1,47 @@
 package sorting;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(final String[] args) {
         Scanner scanner = new Scanner(System.in);
-        List<Long> numbers = new ArrayList<>();
-        while (scanner.hasNextLong()) {
-            numbers.add(scanner.nextLong());
+        DataType dataType = args.length == 2 && args[0].equals("-dataType") ? DataType.valueOf(args[1].toUpperCase()) : DataType.WORD;
+        List<String> list = new ArrayList<>();
+        switch (dataType) {
+            case LONG:
+            case WORD:
+                while (scanner.hasNext()) {
+                    list.add(scanner.next());
+                }
+                long size = list.size();
+                String greatestStr = list.stream().max((s, t) -> {
+                    try {
+                        return Long.valueOf(s).compareTo(Long.valueOf(t));
+                    } catch (NumberFormatException e) {
+                        return Integer.compare(s.length(), t.length());
+                    }
+                }).orElse("");
+                long greatestStrTimes = list.stream().filter(s-> s.equals(greatestStr)).count();
+                long percentage = greatestStrTimes * 100 / size;
+                System.out.println("Total numbers: " + size + '.');
+                System.out.println("The greatest number: " + greatestStr + " (" + greatestStrTimes
+                        + " time(s), " +  percentage + "%).");
+                break;
+            case LINE:
+                while (scanner.hasNextLine()) {
+                    list.add(scanner.nextLine());
+                }
+                size = list.size();
+                greatestStr = list.stream().max(Comparator.comparingInt(String::length)).orElse("");
+                greatestStrTimes = list.stream().filter(s -> s.equals(greatestStr)).count();
+                percentage = greatestStrTimes * 100 / size;
+                System.out.println("Total numbers: " + size + '.');
+                System.out.println("The longest line:\n" + greatestStr + "\n(" + greatestStrTimes
+                        + " time(s), " +  percentage + "%).");
+                break;
         }
-        long size = numbers.size();
-        long greatest = numbers.stream().max(Long::compareTo).orElse(0L);
-        long greatestTimes = numbers.stream().filter(n -> n == greatest).count();
-        System.out.println("Total numbers: " + size + '.');
-        System.out.println("The largest number: " + greatest + " (" + greatestTimes + " time(s)).");
     }
 }
