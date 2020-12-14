65885417084921ec2973856bd462508aee7e0d92
mroui <martynaxroj@gmail.com>
2020-12-14 13:44:54 +0100

    [MODIFY] parsing arguments with error handling

--- Sorting Tool/Sorting Tool/task/src/sorting/Main.java
+++ Sorting Tool/Sorting Tool/task/src/sorting/Main.java
@@ -8,17 +8,22 @@ public class Main {
     public static void main(final String[] args) {
         DataType dataType = DataType.WORD;
         SortingType sortingType = SortingType.NATURAL;
-        try {
-            if (args.length > 0) {
-                for (int i = 0; i < args.length; i += 2) {
-                    if (args[i].toUpperCase().equals(SORTING_TYPE))
-                        sortingType = SortingType.valueOf(args[i + 1].toUpperCase());
-                    else if (args[i].toUpperCase().equals(DATA_TYPE))
-                        dataType = DataType.valueOf(args[i + 1].toUpperCase());
-                }
+        for (int i = 0; i < args.length; i += 2) {
+            if (args[i].toUpperCase().equals(DATA_TYPE))
+                if (args.length == 1 || i == args.length - 1 || args[i + 1].contains("-")) {
+                    System.out.println("No data type defined!");
+                    return;
+                } else dataType = DataType.valueOf(args[i + 1].toUpperCase());
+            else if (args[i].toUpperCase().equals(SORTING_TYPE))
+                if (args.length == 1 || i == args.length - 1 || args[i + 1].contains("-")) {
+                    System.out.println("No sorting type defined!");
+                    return;
+                } else sortingType = SortingType.valueOf(args[i + 1].toUpperCase());
+            else {
+                System.out.println("\"" + args[i] + "\"" + " isn't a valid parameter. It's skipped.");
+                --i;
             }
-        } finally {
-            new SortingTool(dataType, sortingType).sort();
         }
+        new SortingTool(dataType, sortingType).sort();
     }
 }
