559944359443f42f8bc74f01f4cef1227dbedcfa
mroui <martynaxroj@gmail.com>
2020-12-14 17:02:22 +0100

    [MODIFY] extract reading data & add reading/saving data from/to file

--- Sorting Tool/Sorting Tool/task/src/sorting/Main.java
+++ Sorting Tool/Sorting Tool/task/src/sorting/Main.java
@@ -1,11 +1,31 @@
 package sorting;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(final String[] args) {
         Arguments arguments = Arguments.parse(args);
         if (arguments != null) {
-            new SortingTool(arguments.dataType(), arguments.sortingType()).sort();
+            SortingTool sortingTool = new SortingTool(arguments.dataType(), arguments.sortingType());
+            try {
+                if (arguments.inputFile() != null)
+                    sortingTool.readData(new Scanner(Path.of(arguments.inputFile()), StandardCharsets.UTF_8.name()));
+                else sortingTool.readData(new Scanner(System.in));
+            } catch (IOException e) {
+                System.out.println("Error while reading: " + e.toString());
+            }
+            sortingTool.sort();
+            if (arguments.outputFile() != null) {
+                try {
+                    sortingTool.saveData(arguments.outputFile());
+                } catch (IOException e) {
+                    System.out.println("Error while saving: " + e.toString());
+                }
+            }
         }
     }
 
